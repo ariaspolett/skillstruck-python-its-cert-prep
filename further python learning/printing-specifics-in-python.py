f470cf2c76433429c1f09e6269f0139f18a768b1
ariaspolett <ariaspolett21@gmail.com>
2024-10-18 13:20:53 -0700

    exported from Skill Struck

---
+++ further python learning/printing-specifics-in-python.py
@@ -0,0 +1,16 @@
+#Checkpoint - 
+print("How do you write like \n youre running out of time?")
+print("treasury or \t state department?")
+print("history has its \r eyes on you!")
+
+#Challenge - Freshman to Senior
+print("4 \t \t \t Senior")
+print("3 \t \t Junior")
+print("2 \t Sophomore")
+print("1 Freshman")
+
+#Challenge - Once Upon a Time
+print("Once \n Upon \n a \n time")
+
+#Challenge - All for One and One for All!
+print("How write like you \n need it \t to survive?!")
