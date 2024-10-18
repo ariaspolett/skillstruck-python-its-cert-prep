273da3a0c2858a27a15e2af6262c9e96a353ce25
ariaspolett <ariaspolett21@gmail.com>
2024-10-18 12:24:42 -0700

    exported from Skill Struck

--- further python learning/python-math-library.py
+++ further python learning/python-math-library.py
@@ -35,3 +35,12 @@ total = round(sub, 2)
 print(total)
 
 #Challenge - Right Triangle
+import math
+a = int(input("What is the length of side a?"))
+b = int(input("What is the length of side b?"))
+asquared = pow(a,2)
+bsquared = pow(b,2)
+csqaured = asquared + bsquared
+c = math.sqrt(csqaured)
+final = round(c,2)
+print(final)
