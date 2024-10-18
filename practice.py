c5c9d9aace567ba91873d9a16e609378de2e0292
ariaspolett <ariaspolett21@gmail.com>
2024-10-18 12:24:43 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,1 +1,9 @@
-nexxt
+import math
+a = int(input("What is the length of side a?"))
+b = int(input("What is the length of side b?"))
+asquared = pow(a,2)
+bsquared = pow(b,2)
+csqaured = asquared + bsquared
+c = math.sqrt(csqaured)
+final = round(c,2)
+print(final)
