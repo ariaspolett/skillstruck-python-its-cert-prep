63f62a6c7fcad0547075fd1abcc5b84ea07bbdf7
ariaspolett <ariaspolett21@gmail.com>
2024-10-18 13:09:58 -0700

    exported from Skill Struck

--- practice.py
+++ practice.py
@@ -1,9 +1,26 @@
-import math
-a = int(input("What is the length of side a?"))
-b = int(input("What is the length of side b?"))
-asquared = pow(a,2)
-bsquared = pow(b,2)
-csqaured = asquared + bsquared
-c = math.sqrt(csqaured)
-final = round(c,2)
-print(final)
+import datetime
+x = datetime.datetime.now()
+if month == 1:
+    print("Feliz dia de los Reyes Magos!")
+elif month == 2:
+    print("Happy Valentines Day!")
+elif month == 3:
+    print("Happy St.Patrick's Day!")
+elif month == 4:
+    print("Happy Easter!")
+elif month == 5:
+    print("Happy Memorial Day!")
+elif month == 6:
+    print("Happy Juneteenth!")
+elif month == 7:
+    print("Happy Independence Day!")
+elif month == 8:
+    print("Happy American Family Day!")
+elif month == 9:
+    print("Happy Labor Day!")
+elif month == 10:
+    print("Happy Halloween!")
+elif month == 11:
+    print("Happy Thanksgiving!")
+else:
+    print("Merry Christmas!")
